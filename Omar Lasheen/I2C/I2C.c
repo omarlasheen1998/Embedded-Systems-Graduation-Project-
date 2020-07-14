f1a942af1746bb02373965ccf903560af3c8a13e
omarlasheen1998 <63729266+omarlasheen1998@users.noreply.github.com>
2020-07-14 07:51:04 +0200

    update I2C Lasheen

--- Omar Lasheen/I2C/I2C.c
+++ Omar Lasheen/I2C/I2C.c
@@ -1,195 +1,180 @@
 /*
  * CFile1.c
  *
- * Created: 5/21/2020 2:09:29 PM
+ * Created: 14-Jul-20 4:19:24 AM
  *  Author: Lasheen
  */ 
-#define F_CPU 8000000UL
-#include <avr/io.h>
-#include <math.h>
 #include "I2C.h"
-
-#define BITRATE(TWSR)	((F_CPU/SCL_CLK)-16)/(2*pow(4,(TWSR&((1<<TWPS0)|(1<<TWPS1)))))
-
-unsigned char I2C_Read_Ack();
-unsigned char I2C_Read_Nack();
-
-void I2C_Master_Init(int SCL_CLK)
+unsigned int I2C_CLOCK;
+void I2C_Master_Init(unsigned int SCLK)												/* I2C initialize function */
 {
-	
-TWBR = BITRATE(TWSR=0x00);
-
+   	I2C_CLOCK=SCLK;
+	TWBR = BITRATE(TWSR = 0x00);							/* Get bit rate register value by formula */
 }
 
-unsigned char I2C_Start(unsigned char write_address)
+
+uint8_t I2C_Start(char write_address)						/* I2C start function */
 {
-	/* I2C start function */
-	uint8_t status;		/* Declare variable */
-	TWCR=(1<<TWSTA)|(1<<TWEN)|(1<<TWINT); /* Enable TWI, generate START */
-	while(!(TWCR&(1<<TWINT)));	/* Wait until TWI finish its current job */
-	status=TWSR&0xF8;		/* Read TWI status register */
-	if(status!=TW_START)		/* Check weather START transmitted or not? */
-	return 0;			/* Return 0 to indicate start condition fail */
-	TWDR=(write_address<<1)&0xFE;		/* Write SLA+W in TWI data register */
-	TWCR=(1<<TWEN)|(1<<TWINT);	/* Enable TWI & clear interrupt flag */
-	while(!(TWCR&(1<<TWINT)));	/* Wait until TWI finish its current job */
-	status=TWSR&0xF8;		/* Read TWI status register */
-	if(status==TW_MT_SLA_W_ACK)		/* Check for SLA+W transmitted &ack received */
-	return 1;			/* Return 1 to indicate ack received */
-	else if(status==TW_MT_SLA_W_NACK)		/* Check for SLA+W transmitted &nack received */
-	return 2;			/* Return 2 to indicate nack received */
+	uint8_t status;											/* Declare variable */
+	TWCR = (1<<TWSTA)|(1<<TWEN)|(1<<TWINT);					/* Enable TWI, generate start condition and clear interrupt flag */
+	while (!(TWCR & (1<<TWINT)));							/* Wait until TWI finish its current job (start condition) */
+	status = TWSR & 0xF8;									/* Read TWI status register with masking lower three bits */
+	if (status != TW_START)										/* Check weather start condition transmitted successfully or not? */
+	return 0;												/* If not then return 0 to indicate start condition fail */
+	TWDR = write_address;									/* If yes then write SLA+W in TWI data register */
+	TWCR = (1<<TWEN)|(1<<TWINT);							/* Enable TWI and clear interrupt flag */
+	while (!(TWCR & (1<<TWINT)));							/* Wait until TWI finish its current job (Write operation) */
+	status = TWSR & 0xF8;									/* Read TWI status register with masking lower three bits */
+	if (status == TW_MT_SLA_W_ACK)										/* Check weather SLA+W transmitted & ack received or not? */
+	return 1;												/* If yes then return 1 to indicate ack received i.e. ready to accept data byte */
+	if (status == TW_MT_SLA_W_NACK)										/* Check weather SLA+W transmitted & nack received or not? */
+	return 2;												/* If yes then return 2 to indicate nack received i.e. device is busy */
 	else
-	return 3;			/* Else return 3 to indicate SLA+W failed */
+	return 3;												/* Else return 3 to indicate SLA+W failed */
 }
 
-unsigned char I2C_Repeated_Start(unsigned char read_address)
+uint8_t I2C_Repeated_Start(char read_address)				/* I2C repeated start function */
 {
-	 /* I2C repeated start function */
-	
-		unsigned char status;		/* Declare variable */
-		TWCR=(1<<TWSTA)|(1<<TWEN)|(1<<TWINT);/* Enable TWI, generate start */
-		while(!(TWCR&(1<<TWINT)));	/* Wait until TWI finish its current job */
-		status=TWSR&0xF8;		/* Read TWI status register */
-		if(status!=TW_REP_START)		/* Check for repeated start transmitted */
-		return 0;			/* Return 0 for repeated start condition fail */
-		TWDR=(read_address<<1)|0x01;		/* Write SLA+R in TWI data register */
-		TWCR=(1<<TWEN)|(1<<TWINT);	/* Enable TWI and clear interrupt flag */
-		while(!(TWCR&(1<<TWINT)));	/* Wait until TWI finish its current job */
-		status=TWSR&0xF8;		/* Read TWI status register */
-		if(status == TW_MT_SLA_R_ACK)		/* Check for SLA+R transmitted &ack received */
-		return 1;			/* Return 1 to indicate ack received */
-		else if(status == TW_MT_SLA_R_NACK)	/* Check for SLA+R transmitted &nack received */
-		return 2;			/* Return 2 to indicate nack received */
-		else
-		return 3;			/* Else return 3 to indicate SLA+W failed */
-	
-}
-unsigned char I2C_Write(unsigned char data)	
-{
-	/* I2C write function */
-	
-	unsigned char status;		/* Declare variable */
-	TWDR=data;			/* Copy data in TWI data register */
-	TWCR=(1<<TWEN)|(1<<TWINT);	/* Enable TWI and clear interrupt flag */
-	while(!(TWCR&(1<<TWINT)));	/* Wait until TWI finish its current job */
-	status=TWSR&0xF8;		/* Read TWI status register */
-	if(status == TW_MT_DATA_ACK)		/* Check for data transmitted &ack received */
-	return 0;			/* Return 0 to indicate ack received */
-	else if(status == TW_MT_DATA_NACK)		/* Check for data transmitted &nack received */
-	return 1;			/* Return 1 to indicate nack received */
+	uint8_t status;											/* Declare variable */
+	TWCR = (1<<TWSTA)|(1<<TWEN)|(1<<TWINT);					/* Enable TWI, generate start condition and clear interrupt flag */
+	while (!(TWCR & (1<<TWINT)));							/* Wait until TWI finish its current job (start condition) */
+	status = TWSR & 0xF8;									/* Read TWI status register with masking lower three bits */
+	if (status != TW_REP_START)										/* Check weather repeated start condition transmitted successfully or not? */
+	return 0;												/* If no then return 0 to indicate repeated start condition fail */
+	TWDR = read_address;									/* If yes then write SLA+R in TWI data register */
+	TWCR = (1<<TWEN)|(1<<TWINT);							/* Enable TWI and clear interrupt flag */
+	while (!(TWCR & (1<<TWINT)));							/* Wait until TWI finish its current job (Write operation) */
+	status = TWSR & 0xF8;									/* Read TWI status register with masking lower three bits */
+	if (status == TW_MT_SLA_R_ACK)										/* Check weather SLA+R transmitted & ack received or not? */
+	return 1;												/* If yes then return 1 to indicate ack received */
+	if (status == TW_MT_SLA_W_NACK)										/* Check weather SLA+R transmitted & nack received or not? */
+	return 2;												/* If yes then return 2 to indicate nack received i.e. device is busy */
 	else
-	return 2;			/* Else return 2 for data transmission failure */
+	return 3;												/* Else return 3 to indicate SLA+R failed */
 }
 
-unsigned char I2C_Read_Ack()		
+void I2C_Stop()												/* I2C stop function */
 {
-	/* I2C read ack function */
-	TWCR=(1<<TWEN)|(1<<TWINT)|(1<<TWEA); /* Enable TWI, generation of ack */
-	while(!(TWCR&(1<<TWINT)));	/* Wait until TWI finish its current job */
-	return TWDR;			/* Return received data */
+	TWCR=(1<<TWSTO)|(1<<TWINT)|(1<<TWEN);					/* Enable TWI, generate stop condition and clear interrupt flag */
+	while(TWCR & (1<<TWSTO));								/* Wait until stop condition execution */
 }
 
-unsigned char I2C_Read_Nack()		/* I2C read nack function */
+void I2C_Start_Wait(char write_address)						/* I2C start wait function */
 {
-	TWCR=(1<<TWEN)|(1<<TWINT);	/* Enable TWI and clear interrupt flag */
-	while(!(TWCR&(1<<TWINT)));	/* Wait until TWI finish its current job */
-	return TWDR;		       /* Return received data */
-}
-
-void I2C_Stop()			/* I2C stop function */
-{
-	TWCR=(1<<TWSTO)|(1<<TWINT)|(1<<TWEN);/* Enable TWI, generate stop */
-	while(TWCR&(1<<TWSTO));	/* Wait until stop condition execution */
-}
-
-void I2C_Slave_Init(unsigned char slave_address)
-{
-	TWAR=(slave_address<<1)|(1<<TWGCE);		/* Assign Address in TWI address register */
-	TWCR=(1<<TWEN)|(1<<TWEA)|(1<<TWINT);/* Enable TWI, Enable ack generation */
-}
-
-unsigned char I2C_Slave_Listen()
-{
-	while(1)
+	uint8_t status;											/* Declare variable */
+	while (1)
 	{
-		char status;			/* Declare variable */
-		while(!(TWCR&(1<<TWINT)));	/* Wait to be addressed */
-		status=TWSR&0xF8;		/* Read TWI status register */
-		if(status==0x60||status==0x68)	/* Own SLA+W received &ack returned */
-		return 0;			
-		else if(status==0xA8||status==0xB0)	/* Own SLA+R received &ack returned */
-		return 1;		
-		else if(status==0x70||status==0x78)	/* General call received &ack returned */
-		return 2;			/* Return 1 to indicate ack returned */
-		else
-		continue;			/* Else continue */
+		TWCR = (1<<TWSTA)|(1<<TWEN)|(1<<TWINT);				/* Enable TWI, generate start condition and clear interrupt flag */
+		while (!(TWCR & (1<<TWINT)));						/* Wait until TWI finish its current job (start condition) */
+		status = TWSR & 0xF8;								/* Read TWI status register with masking lower three bits */
+		if (status != TW_START)									/* Check weather start condition transmitted successfully or not? */
+		continue;											/* If no then continue with start loop again */
+		TWDR = write_address;								/* If yes then write SLA+W in TWI data register */
+		TWCR = (1<<TWEN)|(1<<TWINT);						/* Enable TWI and clear interrupt flag */
+		while (!(TWCR & (1<<TWINT)));						/* Wait until TWI finish its current job (Write operation) */
+		status = TWSR & 0xF8;								/* Read TWI status register with masking lower three bits */
+		if (status != TW_MT_SLA_W_ACK )								/* Check weather SLA+W transmitted & ack received or not? */
+		{
+			I2C_Stop();										/* If not then generate stop condition */
+			continue;										/* continue with start loop again */
+		}
+		break;												/* If yes then break loop */
 	}
 }
 
-char I2C_Slave_Transmit(unsigned char data){
+uint8_t I2C_Write(char data)								/* I2C write function */
+{
+	uint8_t status;											/* Declare variable */
+	TWDR = data;											/* Copy data in TWI data register */
+	TWCR = (1<<TWEN)|(1<<TWINT);							/* Enable TWI and clear interrupt flag */
+	while (!(TWCR & (1<<TWINT)));							/* Wait until TWI finish its current job (Write operation) */
+	status = TWSR & 0xF8;									/* Read TWI status register with masking lower three bits */
+	if (status == TW_MT_DATA_ACK)										/* Check weather data transmitted & ack received or not? */
+	return 0;												/* If yes then return 0 to indicate ack received */
+	if (status == TW_MT_DATA_NACK)										/* Check weather data transmitted & nack received or not? */
+	return 1;												/* If yes then return 1 to indicate nack received */
+	else
+	return 2;												/* Else return 2 to indicate data transmission failed */
+}
 
-	  char status;
-	  TWDR=data;			/* Write data to TWDR to be transmitted */
-	  TWCR=(1<<TWEN)|(1<<TWINT)|(1<<TWEA);/* Enable TWI & clear interrupt flag */
-	  while(!(TWCR&(1<<TWINT)));	/* Wait until TWI finish its current job */
-	  status=TWSR&0xF8;		/* Read TWI status register */
-	  if(status==TW_SLAVE_ST)		/* Check for STOP/REPEATED START received */
-	  {
-		  TWCR|=(1<<TWINT);	/* Clear interrupt flag & return -1 */
-		  return -1;
-	  }
-	  else if(status == TW_SLAVE_DATA_ACK)		/* Check for data transmitted &ack received */
-	  return 0;			/* If yes then return 0 */
-	  else if(status == TW_SLAVE_DATA_NACK)		/* Check for data transmitted &nack received */
-	  {
-		  TWCR|=(1<<TWINT);	/* Clear interrupt flag & return -2 */
-		  return -2;
-	  }
-	  else if(status == TW_SLAVE_LAST_DATA_ACK)		/* Last byte transmitted with ack received //this is an error as last byte should transmit with nack */
-	  return -3;			/* If yes then return -3 */
-	  else			/* else return -4 */
-	  return -4;
-	
+char I2C_Read_Ack()											/* I2C read ack function */
+{
+	TWCR=(1<<TWEN)|(1<<TWINT)|(1<<TWEA);					/* Enable TWI, generation of ack and clear interrupt flag */
+	while (!(TWCR & (1<<TWINT)));							/* Wait until TWI finish its current job (read operation) */
+	return TWDR;											/* Return received data */
+}
+
+char I2C_Read_Nack()										/* I2C read nack function */
+{
+	TWCR=(1<<TWEN)|(1<<TWINT);								/* Enable TWI and clear interrupt flag */
+	while (!(TWCR & (1<<TWINT)));							/* Wait until TWI finish its current job (read operation) */
+	return TWDR;											/* Return received data */
+}
+
+void I2C_Slave_Init(uint8_t slave_address)
+{
+	TWAR = slave_address;						/* Assign address in TWI address register */
+	TWCR = (1<<TWEN) | (1<<TWEA) | (1<<TWINT);	/* Enable TWI, Enable ack generation, clear TWI interrupt */
+}
+
+int8_t I2C_Slave_Listen()
+{
+	while(1)
+	{
+		uint8_t status;							/* Declare variable */
+		while (!(TWCR & (1<<TWINT)));			/* Wait to be addressed */
+		status = TWSR & 0xF8;					/* Read TWI status register with masking lower three bits */
+		if (status == 0x60 || status == 0x68)	/* Check weather own SLA+W received & ack returned (TWEA = 1) */
+		return 0;								/* If yes then return 0 to indicate ack returned */
+		if (status == 0xA8 || status == 0xB0)	/* Check weather own SLA+R received & ack returned (TWEA = 1) */
+		return 1;								/* If yes then return 1 to indicate ack returned */
+		if (status == 0x70 || status == 0x78)	/* Check weather general call received & ack returned (TWEA = 1) */
+		return 2;								/* If yes then return 2 to indicate ack returned */
+		else
+		continue;								/* Else continue */
+	}
+}
+
+int8_t I2C_Slave_Transmit(char data)
+{
+	uint8_t status;
+	TWDR = data;								/* Write data to TWDR to be transmitted */
+	TWCR = (1<<TWEN)|(1<<TWINT)|(1<<TWEA);		/* Enable TWI and clear interrupt flag */
+	while (!(TWCR & (1<<TWINT)));				/* Wait until TWI finish its current job (Write operation) */
+	status = TWSR & 0xF8;						/* Read TWI status register with masking lower three bits */
+	if (status == TW_SLAVE_ST)							/* Check weather STOP/REPEATED START received */
+	{
+		TWCR |= (1<<TWINT);						/* If yes then clear interrupt flag & return -1 */
+		return -1;
+	}
+	if (status == TW_SLAVE_DATA_ACK)							/* Check weather data transmitted & ack received */
+	return 0;									/* If yes then return 0 */
+	if (status == TW_SLAVE_DATA_NACK)							/* Check weather data transmitted & nack received */
+	{
+		TWCR |= (1<<TWINT);						/* If yes then clear interrupt flag & return -2 */
+		return -2;
+	}
+	if (status == TW_SLAVE_LAST_DATA_ACK)							/* If last data byte transmitted with ack received TWEA = 0 */
+	return -3;									/* If yes then return -3 */
+	else										/* else return -4 */
+	return -4;
 }
 
 char I2C_Slave_Receive()
 {
-	char status;		/* Declare variable */
-	TWCR=(1<<TWEN)|(1<<TWEA)|(1<<TWINT);/* Enable TWI & generation of ack */
-	while(!(TWCR&(1<<TWINT)));	/* Wait until TWI finish its current job */
-	status=TWSR&0xF8;		/* Read TWI status register */
-	if(status==0x80||status==0x90)/* Check for data received &ack returned */
-	return TWDR;		/* If yes then return received data */
-
-	/* Check for data received, nack returned & switched to not addressed slave mode */
-	if(status==0x88||status==0x98)
-	return TWDR;		/* If yes then return received data */
-	if(status==TW_SLAVE_ST)		/* Check wether STOP/REPEATED START */
+	uint8_t status;								/* Declare variable */
+	TWCR=(1<<TWEN)|(1<<TWEA)|(1<<TWINT);		/* Enable TWI, generation of ack and clear interrupt flag */
+	while (!(TWCR & (1<<TWINT)));				/* Wait until TWI finish its current job (read operation) */
+	status = TWSR & 0xF8;						/* Read TWI status register with masking lower three bits */
+	if (status == 0x80 || status == 0x90)		/* Check weather data received & ack returned (TWEA = 1) */
+	return TWDR;								/* If yes then return received data */
+	if (status == 0x88 || status == 0x98)		/* Check weather data received, nack returned and switched to not addressed slave mode */
+	return TWDR;								/* If yes then return received data */
+	if (status == TW_SLAVE_ST)							/* Check weather STOP/REPEATED START received */
 	{
-		TWCR|=(1<<TWINT);	/* Clear interrupt flag & return -1 */
+		TWCR |= (1<<TWINT);						/* If yes then clear interrupt flag & return 0 */
 		return -1;
 	}
 	else
-	return -2;			/* Else return -2 */
-}
-
-char I2C_Read(unsigned char no)
-{
-	
-	static unsigned char i=0;
-	
-	if(i<no)
-	{
-		i++;
-		return I2C_Read_Ack();
-	}
-	
-	else if(i==(no-1))
-	{
-		i=0;
-		return I2C_Read_Nack();
-	}
-
-return (-127);	
-
+	return -2;									/* Else return 1 */
 }
--- Omar Lasheen/I2C/I2C.h
+++ Omar Lasheen/I2C/I2C.h
@@ -1,7 +1,7 @@
 /*
  * IncFile1.h
  *
- * Created: 5/21/2020 2:08:56 PM
+ * Created: 14-Jul-20 4:19:02 AM
  *  Author: Lasheen
  */ 
 
@@ -9,9 +9,18 @@
 #ifndef I2C_H_
 #define I2C_H_
 
-/* I2C Status Bits in the TWSR Register */
+#define F_CPU 1000000UL							/* Define CPU clock Frequency e.g. here its 8MHz */
+#include <avr/io.h>								/* Include AVR std. library file */
+#include <util/delay.h>							/* Include inbuilt defined Delay header file */
+#include <math.h>								/* Include math function */
+#include <stdint.h>
 
-//For MASTER 
+
+#define BITRATE(TWSR)	((F_CPU/I2C_CLOCK)-16)/(2*pow(4,(TWSR&((1<<TWPS0)|(1<<TWPS1))))) /* Define bit rate */
+extern unsigned int I2C_CLOCK;
+
+
+//I2C Status 
 #define TW_START         0x08 // start has been sent
 #define TW_REP_START     0x10 // repeated start
 #define TW_MT_SLA_W_ACK  0x18 // Master transmit ( slave address + Write request ) to slave + Ack received from slave
@@ -22,20 +31,27 @@
 #define TW_MT_DATA_NACK  0x30 // Master transmit data and NACK has been received from Slave.
 #define TW_MR_DATA_ACK   0x50 // Master received data and send ACK to slave
 #define TW_MR_DATA_NACK  0x58 // Master received data but doesn't send ACK to slave
-#define TW_SLAVE_ST   0xA0 // Check for STOP/REPEATED START received 
-#define TW_SLAVE_DATA_ACK   0xB8  // Check for slave data transmitted & ack received 
+#define TW_SLAVE_ST   0xA0 // Check for STOP/REPEATED START received
+#define TW_SLAVE_DATA_ACK   0xB8  // Check for slave data transmitted & ack received
 #define TW_SLAVE_DATA_NACK  0xC0 // Check for slave data transmitted & nack received
 #define TW_SLAVE_LAST_DATA_ACK  0xC8 //Last slave data byte in TWDR has been transmitted (TWEA = “0”); ACK has been received
- 	  
-void I2C_Master_Init(int SCL_CLK);
-void I2C_Slave_Init(unsigned char slave_address);
-unsigned char I2C_Start(unsigned char write_address);
-unsigned char I2C_Repeated_Start(unsigned char read_address);
-unsigned char I2C_Write(unsigned char data);
-char I2C_Read(unsigned char no);
-void I2C_Stop()	;
-unsigned char I2C_Slave_Listen();
-char I2C_Slave_Transmit(unsigned char data);
-char I2C_Slave_Receive();
+
+
+
+void I2C_Master_Init(unsigned int SCLK);								/* I2C initialize function */
+uint8_t  I2C_Start(char write_address);			/* I2C start function */
+uint8_t  I2C_Repeated_Start(char read_address);	/* I2C repeated start function */
+void I2C_Stop();								/* I2C stop function */
+void I2C_Start_Wait(char write_address);		/* I2C start wait function */
+uint8_t  I2C_Write(char data);					/* I2C write function */
+char I2C_Read_Ack();							/* I2C read ack function */
+char I2C_Read_Nack();							/* I2C read nack function */
+
+void I2C_Slave_Init(uint8_t slave_address);		/* I2C slave initialize function with Slave address */
+int8_t I2C_Slave_Listen();						/* I2C slave listen function */
+int8_t I2C_Slave_Transmit(char data);			/* I2C slave transmit function */
+char I2C_Slave_Receive();						/* I2C slave receive function */
+
+
 
 #endif /* INCFILE1_H_ */
