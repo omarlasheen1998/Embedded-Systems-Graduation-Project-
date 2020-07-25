b70ef37140bc9d37804f3d0d06da8057e7af3758
Ahmed Ismail <ahmedaass2311@gmail.com>
2020-07-26 00:50:50 +0200

    LCD updated

--- Ahmed Adel/LCD/LCD.cpp
+++ Ahmed Adel/LCD/LCD.cpp
@@ -7,46 +7,192 @@
 
 #include "LCD.h"
 
+LCD::LCD(uint8_t config){
+	if (config >= 0 && config <= 2)
+	{
+		configType = config;
+	}
+}
+
 void LCD::command(uint8_t cmd){
-	LCD_DPRT = cmd;
-	LCD_CPRT &= ~(1 << LCD_RS);
-	LCD_CPRT &= ~(1 << LCD_RW);
-	LCD_CPRT |= (1 << LCD_EN);
+	if (configType == LCD_8_BIT)
+	{
+		LCD_DPRT = cmd;
+		LCD_CPRT &= ~(1 << LCD_RS);
+		LCD_CPRT &= ~(1 << LCD_RW);
+		LCD_CPRT |= (1 << LCD_EN);
+		
+		_delay_us(1);
+		LCD_CPRT &= ~(1 << LCD_EN);
+		_delay_us(100);
+	}
+	else if (configType == LCD_4_BIT)
+	{
+		LCD_PRT = (LCD_PRT & 0x0F) | (cmd & 0xF0); 
+		LCD_PRT &= ~(1 << LCD_RS);
+		LCD_PRT &= ~(1 << LCD_RW);
+		LCD_PRT |= (1 << LCD_EN);
+		
+		_delay_us(1);
+		LCD_PRT &= ~(1 << LCD_EN);
+		_delay_us(100);
+		
+		LCD_PRT = (LCD_PRT & 0x0F) | (cmd << 4); 
+		LCD_PRT |= (1 << LCD_EN);
+		_delay_us(1);
+		LCD_PRT &= ~(1 << LCD_EN);
+		_delay_us(100);
+	}
+	else if (configType == LCD_I2C)
+	{
+		I2C::beginTransmission(LCD_I2C_ADDR);
+		
+		I2C_PRT = (I2C_PRT & 0x0F) | (cmd & 0xF0);
+		I2C::write(I2C_PRT);
+		I2C_PRT &= ~(1 << LCD_RS);
+		I2C::write(I2C_PRT);
+		I2C_PRT &= ~(1 << LCD_RW);
+		I2C::write(I2C_PRT);
+		I2C_PRT |= (1 << LCD_EN);
+		I2C::write(I2C_PRT);
+		
+		_delay_us(1);
+		I2C_PRT &= ~(1 << LCD_EN);
+		I2C::write(I2C_PRT);
+		_delay_us(100);
+		
+		I2C_PRT = (I2C_PRT & 0x0F) | (cmd << 4);
+		I2C::write(I2C_PRT);
+		I2C_PRT |= (1 << LCD_EN);
+		I2C::write(I2C_PRT);
+		
+		_delay_us(1);
+		I2C_PRT &= ~(1 << LCD_EN);
+		I2C::write(I2C_PRT);
+		_delay_us(100);
+		
+		I2C::endTransmission();
+		
+	}
 	
-	_delay_us(1);
-	LCD_CPRT &= ~(1 << LCD_EN);
-	_delay_us(100);
 	
 }
 
 
 void LCD::Init(){
-	LCD_DDDR = 0xFF;
-	LCD_CDDR = 0xFF;
-	
-	LCD_CPRT &= ~(1 << LCD_EN);
-	
-	_delay_us(2000);
-	command(0x38);
-	command(0x0E);
-	command(0x01);
-	
-	_delay_us(2000);
-	command(0x06);
-	
-	
+	if (configType == LCD_8_BIT)
+	{
+		LCD_DDDR = 0xFF;
+		LCD_CDDR = 0xFF;
+		
+		LCD_CPRT &= ~(1 << LCD_EN);
+		
+		_delay_us(2000);
+		command(0x38);
+		command(0x0E);
+		command(0x01);
+		
+		_delay_us(2000);
+		command(0x06);
+	}
+	else if (configType == LCD_4_BIT)
+	{
+		LCD_DDR = 0xFF;
+
+		LCD_PRT &= ~(1 << LCD_EN);
+		
+		_delay_us(2000);
+		command(0x33);
+		command(0x32);
+		command(0x28);
+		command(0x0E);
+		command(0x01);
+		
+		_delay_us(2000);
+		command(0x06);
+	}
+	else if (configType == LCD_I2C)
+	{
+		I2C::begin(TYPE_MASTER);
+		I2C::beginTransmission(LCD_I2C_ADDR);
+				
+		I2C_PRT &= ~(1 << LCD_EN);
+		I2C::write(I2C_PRT);
+		
+		I2C::endTransmission();
+		
+		_delay_us(2000);
+		command(0x33);
+		command(0x32);
+		command(0x28);
+		command(0x0E);
+		command(0x01);
+		
+		_delay_us(2000);
+		command(0x06);
+		
+		
+	}
 }
 
 
 void LCD::printData(uint8_t data){
-	LCD_DPRT = data;
-	LCD_CPRT |= (1 << LCD_RS);
-	LCD_CPRT &= ~(1 << LCD_RW);
-	LCD_CPRT |= (1 << LCD_EN);
+	if (configType == LCD_8_BIT)
+	{
+		LCD_DPRT = data;
+		LCD_CPRT |= (1 << LCD_RS);
+		LCD_CPRT &= ~(1 << LCD_RW);
+		LCD_CPRT |= (1 << LCD_EN);
+		
+		_delay_us(1);
+		LCD_CPRT &= ~(1 << LCD_EN);
+		_delay_us(100);	
+	}
+	else if (configType == LCD_4_BIT)
+	{
+		LCD_PRT = (LCD_PRT & 0x0F) | (data & 0xF0);
+		LCD_PRT |= (1 << LCD_RS);
+		LCD_PRT &= ~(1 << LCD_RW);
+		LCD_PRT |= (1 << LCD_EN);
+		
+		_delay_us(1);
+		LCD_PRT &= ~(1 << LCD_EN);
+		
+		LCD_PRT = (LCD_PRT & 0x0F) | (data << 4);
+		LCD_PRT |= (1 << LCD_EN);
+		_delay_us(1);
+		LCD_PRT &= ~(1 << LCD_EN);
+		_delay_us(100);
+		
+	}
+	else if(configType == LCD_I2C){
+		I2C::beginTransmission(LCD_I2C_ADDR);
+		
+		I2C_PRT = (I2C_PRT & 0x0F) | (data & 0xF0);
+		I2C::write(I2C_PRT);
+		I2C_PRT |= (1 << LCD_RS);
+		I2C::write(I2C_PRT);
+		I2C_PRT &= ~(1 << LCD_RW);
+		I2C::write(I2C_PRT);
+		I2C_PRT |= (1 << LCD_EN);
+		I2C::write(I2C_PRT);
+		
+		_delay_us(1);
+		I2C_PRT &= ~(1 << LCD_EN);
+		I2C::write(I2C_PRT);
+		
+		I2C_PRT = (I2C_PRT & 0x0F) | (data << 4);
+		I2C::write(I2C_PRT);
+		I2C_PRT |= (1 << LCD_EN);
+		I2C::write(I2C_PRT);
+		_delay_us(1);
+		I2C_PRT &= ~(1 << LCD_EN);
+		I2C::write(I2C_PRT);
+		_delay_us(100);
+		
+		I2C::endTransmission();
+	}
 	
-	_delay_us(1);
-	LCD_CPRT &= ~(1 << LCD_EN);
-	_delay_us(100);
 	
 }
 
--- Ahmed Adel/LCD/LCD.h
+++ Ahmed Adel/LCD/LCD.h
@@ -14,22 +14,40 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "GPIO.h"
+#include "I2C.h"
 
-#define AUTO(var, expr) decltype(expr) var = (expr)
+// LCD I2C Settings 
+#define LCD_I2C_ADDR 0x20
 
+// LCD 4-Bit Settings 
+#define LCD_PRT PORTA
+#define LCD_DDR DDRA
+#define LCD_PIN PINA
+
+// LCD 8-Bit Settings
 #define LCD_DPRT PORTC
 #define LCD_DDDR DDRC
 #define LCD_DPIN PINC
 #define LCD_CPRT PORTB
 #define LCD_CDDR DDRB
 #define LCD_CPIN PINB
+
+// Common Settings
 #define LCD_RS 0
 #define LCD_RW 1
 #define LCD_EN 2
 
+// Configurations
+#define LCD_8_BIT 0
+#define LCD_4_BIT 1
+#define LCD_I2C 2
+
+
+
 
 class LCD {
 	public:
+	LCD(uint8_t config);
 	void Init(void);
 	void printString(char *str);
 	void gotoXY(uint8_t x, uint8_t y);
@@ -38,8 +56,8 @@ class LCD {
 	void clear(void);
 	
 	private:
-	
-	
+	uint8_t configType = 0;
+	uint8_t I2C_PRT = 0x00;
 
 
 };
