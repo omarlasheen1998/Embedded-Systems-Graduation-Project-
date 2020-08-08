b8648cb76241b0b187462ec6b1b047b73ebcd16f
Abdelrahman Hosny <67981104+Abdelrahman-Hosny@users.noreply.github.com>
2020-08-08 22:59:17 +0200

    Update Keypad.c

--- Abdelrahman Ibrahim/Keypad/Keypad.c
+++ Abdelrahman Ibrahim/Keypad/Keypad.c
@@ -16,7 +16,7 @@
 
 
 /*** Define Names for 4*4 keypad ***/
-u8 PinsArr[RowNum][ColNum] ={{'7','8','9','A'},{'4','5','6','B'},{'1','2','3','C'},{'*','0','#','D'}};
+u8 PinsArr[RowNum][ColNum] ={{'1','2','3','A'},{'4','5','6','B'},{'7','8','9','C'},{'*','0','#','D'}};
 //(RowNum 4) & (ColNum 4) are defined in Keypad_def.h
 
 
@@ -50,38 +50,46 @@ void KPD_PinsInitialization()
 //FUNC_2 .. Get pressed key
 //Last PressedKey Value remains saved until a new press occurs
 //returns the Value of the Key as Char
+/* How it works 
+ * put zero in a column and check from row if 
+ * if a button is pressed it will read the zero 
+ * if not pressed it will read high from the pull up
+ */
 u8 KPD_GetPressedKey()
 {
-	u8 u8_ColIndex,u8_RowIndex;
-	u8 u8_PinValue = 1;
+	u8 u8_ColIndex,u8_RowIndex; //for looping 
+	u8 u8_PinValue = 1; //initial value 1, this variable will check if the button is pressed or not 
 	//Last PressedKey Value remains saved until a new press occurs
-	u8 u8_PressedKeyValue = '@';  //The returned value
+	u8 u8_PressedKeyValue = '@';  //The returned value, initially = '@'
 
 	for(u8_ColIndex =0 ; u8_ColIndex < ColNum ; u8_ColIndex++)
 			{
-				DIO_SetPinValue(ColPort, u8_ColIndex,low);
+				DIO_SetPinValue(ColPort, u8_ColIndex,low);  //write 0 and check from rows if any button is pressed
 				for (u8_RowIndex = 0;u8_RowIndex < RowNum; u8_RowIndex ++)
 				{
 					DIO_GetPinValue(RowPort,u8_RowIndex,& u8_PinValue);
-					if (u8_PinValue == 0)
+					if (u8_PinValue == 0)    //if (button is pressed)
 					{
-						while(u8_PinValue == 0)
+						while(u8_PinValue == 0)      //Are you are still pressing? .. if yes, wait until you lift your finger 
 						{
-							DIO_GetPinValue(RowPort,u8_RowIndex,& u8_PinValue);
+							DIO_GetPinValue(RowPort,u8_RowIndex,& u8_PinValue);  //i'm holding the value as you are still pressing
+							//Disadvantage .. The proccessor in holding!
+							//another method .. switch cases and checking the prestate
 						}
-						(u8_PressedKeyValue) = PinsArr[u8_RowIndex][u8_ColIndex];
+						(u8_PressedKeyValue) = PinsArr[u8_RowIndex][u8_ColIndex];  //pick the char from the array
 
-						_delay_ms(20);
+						_delay_ms(20);  //Debouncing
 
 					}
-					else{
+					else{ 
+					u8_PressedKeyValue = '@'; //Optional
 					}
 				}
-				DIO_SetPinValue(ColPort, u8_ColIndex,high);
+				DIO_SetPinValue(ColPort, u8_ColIndex,high); //remove the zero you used 
 			}
 
 
 return u8_PressedKeyValue ;
 }
 
-
+//To take numbers of x digits (like passwords or RPM value) .. use for loop with x counts and store values in array of size x using the index
