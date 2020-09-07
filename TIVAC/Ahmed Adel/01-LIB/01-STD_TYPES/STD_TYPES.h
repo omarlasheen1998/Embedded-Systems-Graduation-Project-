59a7d5f86f8f65ac720be7286a2e5d2eb7e652bc
Ahmed Ismail <ahmedaass2311@gmail.com>
2020-09-08 01:12:53 +0200

    TIVA C GPIO updated

---
+++ TIVAC/Ahmed Adel/01-LIB/01-STD_TYPES/STD_TYPES.h
@@ -0,0 +1,16 @@
+/******************************************/
+/* Author 	: Ahmed Ismail				  */
+/* Date		: 29 AUG 2020 				  */
+/* Version 	: V01						  */
+/******************************************/
+
+
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+typedef unsigned char 		uint8;
+typedef unsigned short int 	uint16;
+typedef unsigned long int 	uint32;
+
+
+#endif
