fd32217c09228ca572e645fd3ca11fe8e64cb7af
Omar Lasheen <63729266+omarlasheen1998@users.noreply.github.com>
2021-01-30 04:17:41 +0200

    AFIO update

--- STM32/Omar Lasheen/02-MCAL/05-AFIO/AFIO_interface.h
+++ STM32/Omar Lasheen/02-MCAL/05-AFIO/AFIO_interface.h
@@ -3,13 +3,21 @@
 /* Version   : V01                                                               */
 /* Date      : 11 OCT  2020                                                      */
 /*********************************************************************************/
+#include "STD_TYPES.h"
+
 #ifndef AFIO_INTERFACE_H
 #define AFIO_INTERFACE_H
+
+#define UART1 1
+#define CAN 2
+#define I2C1 3
+#define SPI1 4
 
 
 
 /***************   Function definition    ******************/
 
-void AFIO_voidSetEXTIConfiguration (uint8 Copy_u8EXTI_Line, uint8 Copy_u8PortMap);
+void AFIO_voidSetEXTIConfiguration (u8 Copy_u8EXTI_Line, u8 Copy_u8PortMap);
+void AFIO_voidRemapPeripheralPins (u8 Copy_u8Port);
 
 #endif // AFIO_INTERFACE_H
--- STM32/Omar Lasheen/02-MCAL/05-AFIO/AFIO_program.c
+++ STM32/Omar Lasheen/02-MCAL/05-AFIO/AFIO_program.c
@@ -9,13 +9,14 @@
 #include "AFIO_interface.h"
 #include "AFIO_config.h"
 #include "AFIO_private.h"
+#include "GPIO_private.h"
 
 
 /*************	 Function OF Set EXTI Configuration  **************************/
 
-void AFIO_voidSetEXTIConfiguration (uint8 Copy_u8Line, uint8 Copy_u8PortMap)
+void AFIO_voidSetEXTIConfiguration (u8 Copy_u8Line, u8 Copy_u8PortMap)
 {
-	uint8 Local_u8RegIndex = 0;
+	u8 Local_u8RegIndex = 0;
 	/* Assign to EXTICRx register */
 	Local_u8RegIndex = Copy_u8Line / 4;
 	Copy_u8Line = Copy_u8Line % 4 ;
@@ -25,3 +26,40 @@ void AFIO_voidSetEXTIConfiguration (uint8 Copy_u8Line, uint8 Copy_u8PortMap)
 }
 
 
+void AFIO_voidRemapPeripheralPins (u8 Copy_u8Port){
+
+switch(Copy_u8Port){
+
+	 case UART1:
+		AFIO->MAPR|=1<<2;
+    GPIOA_CRH&=~0x00000FF0;
+    GPIOA_CRH|= 0x00000440; 	// RX1=input with pull-up, TX1=alt. func output	
+	 GPIOB_CRL   &= ~(0xFF000000UL);
+	 GPIOB_CRL   |= 0x8B000000UL;			// Configure PB6 and PB7
+	 GPIOB_ODR |= 0x1UL << 7;
+	 break;
+	case CAN:
+	AFIO->MAPR   &= 0xFFFF9FFF;   	// reset CAN remap
+	AFIO->MAPR   |= 0x00004000;   	//  et CAN remap, use PB8, PB9	
+	
+	GPIOB_CRH   &= ~(0xFFUL);
+	GPIOB_CRH   |= 0xB8UL;			// Configure PB8 and PB9
+	GPIOB_ODR |= 0x1UL << 8;
+	break;
+	case I2C1:
+		AFIO->MAPR|=1<<1;
+	 break;
+	case SPI1:
+		AFIO->MAPR|=1<<0;
+	 break;
+	
+
+
+
+
+}
+
+
+
+
+}
