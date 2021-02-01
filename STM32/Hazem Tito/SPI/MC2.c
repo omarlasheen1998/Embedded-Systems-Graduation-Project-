e68952fc5afc4221e02e031e26f6b895ebe323d3
Hazemmagdy1998 <77415361+Hazemmagdy1998@users.noreply.github.com>
2021-02-01 03:33:55 +0200

    .

---
+++ STM32/Hazem Tito/SPI/MC2.c
@@ -0,0 +1,16 @@
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "RCC_interface.h"
+#include "RCC_private.h"
+#include "RCC_config.h"
+#include "SPI_interface.h"
+#include "SPI_private.h"
+#include "SPI_config.h"
+
+int main(void){
+uint8 character;
+RCC_APB2ENR|=0xFC;
+SPI_voidInitSlave(SPI_1); /* SLAVE IS A STM MICROCONTROLLER*/
+character=SPI_u8RecieveByte(SPI_1);
+
+}
